c8c4f93f1acd55b88bf1078bcca3f994c7801774
merckeu <merckeu@gmail.com>
2024-12-28 15:20:08 -0400

    Update config.php

--- as_digital/config.php
+++ as_digital/config.php
@@ -3,7 +3,7 @@
 // Configurações gerais do addon
 return [
     'addon_name' => 'Digital Signature Addon',
-    'version' => '1.0.0',
+    'version' => '1.2.0',
     'author' => 'Merckeu M Moretti',
     'description' => 'Addon para assinatura digital e manuscrita no MK-Auth.',
     'signature_dir' => '/opt/mk-auth/admin/addons/as_digital/uploads/signatures/', // Caminho para salvar as assinaturas
